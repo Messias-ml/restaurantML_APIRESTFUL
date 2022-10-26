929dc878333ec1cc1f8f9753f922b4d68c76dc4d
Messias Silva <messiasvedita@hotmail.com>
2022-10-26 09:00:51 -0300

    refatorando teste end-to-end

--- src/main/resources/db/migration/V003__refactor_address.sql
+++ src/main/resources/db/migration/V003__refactor_address.sql
@@ -1,25 +1,25 @@
-ALTER TABLE `restaurantesml`.`restaurant`
+ALTER TABLE `restaurant`
 DROP FOREIGN KEY `FK_city`;
-ALTER TABLE `restaurantesml`.`restaurant`
+ALTER TABLE `restaurant`
 CHANGE COLUMN `address_neighborhood` `address_neighborhood` VARCHAR(255) NULL ,
 CHANGE COLUMN `address_number` `address_number` SMALLINT NULL ,
 CHANGE COLUMN `address_patio` `address_patio` VARCHAR(100) NULL ,
 CHANGE COLUMN `address_zip_cod` `address_zip_cod` VARCHAR(10) NULL ,
 CHANGE COLUMN `address_id_city` `address_id_city` BIGINT NULL ;
-ALTER TABLE `restaurantesml`.`restaurant`
+ALTER TABLE `restaurant`
 ADD CONSTRAINT `FK_city`
   FOREIGN KEY (`address_id_city`)
-  REFERENCES `restaurantesml`.`city` (`id_city`);
+  REFERENCES `city` (`id_city`);
 
-  ALTER TABLE `restaurantesml`.`demand`
+  ALTER TABLE `demand`
   DROP FOREIGN KEY `fk_demand_address_city`;
-  ALTER TABLE `restaurantesml`.`demand`
+  ALTER TABLE `demand`
   CHANGE COLUMN `address_neighborhood` `address_neighborhood` VARCHAR(255) NULL ,
   CHANGE COLUMN `address_number` `address_number` SMALLINT NULL ,
   CHANGE COLUMN `address_patio` `address_patio` VARCHAR(100) NULL ,
   CHANGE COLUMN `address_zip_cod` `address_zip_cod` VARCHAR(10) NULL ,
   CHANGE COLUMN `address_id_city` `address_id_city` BIGINT NULL ;
-  ALTER TABLE `restaurantesml`.`demand`
+  ALTER TABLE `demand`
   ADD CONSTRAINT `fk_demand_address_city`
     FOREIGN KEY (`address_id_city`)
-    REFERENCES `restaurantesml`.`city` (`id_city`);
+    REFERENCES `city` (`id_city`);
