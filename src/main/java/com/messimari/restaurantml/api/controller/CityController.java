0ee2d6d5cf6f2937c4d5194d41a59d5493299682
Messias Silva <messiasvedita@hotmail.com>
2023-02-08 10:27:35 -0300

    atualizando swagger de cidade.

--- src/main/java/com/messimari/restaurantml/api/controller/CityController.java
+++ src/main/java/com/messimari/restaurantml/api/controller/CityController.java
@@ -5,6 +5,8 @@ import com.messimari.restaurantml.api.model.dto.city.CityRequestDTO;
 import com.messimari.restaurantml.api.model.dto.city.CityResponseDTO;
 import com.messimari.restaurantml.domain.model.CityEntity;
 import com.messimari.restaurantml.domain.service.RegistrationCityService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,29 +15,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
+@Api(tags = "City")
 public class CityController {
 
     @Autowired
     private RegistrationCityService service;
 
+    @ApiOperation("Create City")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void createCity(@RequestBody CityRequestDTO city){
         service.createCity(city);
     }
 
+    @ApiOperation("find list of Cities")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<CityDTO> findlistCities(){
         return service.findlistCities();
     }
 
+    @ApiOperation("find by id City")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public CityResponseDTO findByIdcity (@PathVariable("id") Long id){
         return service.findByIdcity(id);
     }
 
+    @ApiOperation("Update City")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void updateCity(@PathVariable Long id,
@@ -43,6 +50,7 @@ public class CityController {
         service.updateCity(id, updatedCity);
     }
 
+    @ApiOperation("Delete City")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteCity(@PathVariable("id") Long id){
--- src/main/java/com/messimari/restaurantml/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/messimari/restaurantml/core/springfox/SpringFoxConfig.java
@@ -2,7 +2,11 @@ package com.messimari.restaurantml.core.springfox;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,6 +17,17 @@ public class SpringFoxConfig {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("City", "City of state"));
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("RestaurantML")
+                .description("Api de restaurant")
+                .version("1")
+                .contact(new Contact("Messias", "https://www.linkedin.com/in/messias-dev-silva/","messias.silva_dev@hotmail.com"))
                 .build();
     }
 }
