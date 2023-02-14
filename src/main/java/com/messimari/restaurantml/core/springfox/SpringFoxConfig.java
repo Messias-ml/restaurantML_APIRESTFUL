5722d4ffb83861410dfa57594c0644c424f7b44d
Messias Silva <messiasvedita@hotmail.com>
2023-02-14 15:33:45 -0300

    Adicionando status de erro gerais no swagger

--- src/main/java/com/messimari/restaurantml/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/messimari/restaurantml/core/springfox/SpringFoxConfig.java
@@ -2,13 +2,21 @@ package com.messimari.restaurantml.core.springfox;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @Configuration
 public class SpringFoxConfig {
@@ -18,16 +26,103 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .build()
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
+                .globalResponses(HttpMethod.POST, globalPostResponseMessages())
+                .globalResponses(HttpMethod.PUT, globalPutResponseMessages())
+                .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("City", "City of state"));
     }
 
-    public ApiInfo apiInfo(){
+    private List<Response> globalPostResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                        .description("Recurso não encontrado ou não existente.")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Envio de formulario com erro, não teve os preenchimentos necessario ou algo do tipo.")
+                        .build()
+        );
+    }
+
+    private List<Response> globalPutResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                        .description("Recurso não encontrado ou não existente.")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Envio de formulario com erro, não teve os preenchimentos necessario ou algo do tipo.")
+                        .build()
+        );
+    }
+
+    private List<Response> globalDeleteResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.CONFLICT.value()))
+                        .description("Recurso não pode ser excluido por ja esta sendo usado")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                        .description("Recurso não encontrado ou não existente.")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Envio de formulario com erro, não teve os preenchimentos necessario ou algo do tipo.")
+                        .build()
+        );
+    }
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                        .description("Recurso não encontrado ou não existente.")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Envio de formulario com erro, não teve os preenchimentos necessario ou algo do tipo.")
+                        .build()
+        );
+    }
+
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("RestaurantML")
                 .description("Api de restaurant")
                 .version("1")
-                .contact(new Contact("Messias", "https://www.linkedin.com/in/messias-dev-silva/","messias.silva_dev@hotmail.com"))
+                .contact(new Contact("Messias", "https://www.linkedin.com/in/messias-dev-silva/", "messias.silva_dev@hotmail.com"))
                 .build();
     }
 }
